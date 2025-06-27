4bce5644557777c39175453841bf20cac2a21f46
Mia Koring <koring.mia@gmail.com>
2025-06-27 20:39:26 +0200

    default image provider is now kingfisher to support gifs

--- Sources/MarkdownUI/Extensibility/DefaultImageProvider.swift
+++ Sources/MarkdownUI/Extensibility/DefaultImageProvider.swift
@@ -1,20 +1,24 @@
 import NetworkImage
 import SwiftUI
+import Kingfisher
 
 /// The default image provider, which loads images from the network.
-public struct DefaultImageProvider: ImageProvider {
-  public func makeImage(url: URL?) -> some View {
-    NetworkImage(url: url) { state in
-      switch state {
-      case .empty, .failure:
-        Color.clear
-          .frame(width: 0, height: 0)
-      case .success(let image, let idealSize):
-        ResizeToFit(idealSize: idealSize) {
-          image.resizable()
-        }
-      }
-    }
+public struct DefaultImageProvider: @preconcurrency ImageProvider {
+    @MainActor public func makeImage(url: URL?) -> some View {
+        KFAnimatedImage(url)
+            .scaledToFit()
+        /*
+        NetworkImage(url: url) { state in
+              switch state {
+              case .empty, .failure:
+                Color.clear
+                  .frame(width: 0, height: 0)
+              case .success(let image, let idealSize):
+                ResizeToFit(idealSize: idealSize) {
+                  image.resizable()
+                }
+              }
+            }*/
   }
 }
 
